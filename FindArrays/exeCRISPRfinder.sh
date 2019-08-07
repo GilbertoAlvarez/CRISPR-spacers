ef203dd588cf38618f98ec1d88fd912a4f286c79
Gilberto Álvarez <37610463+GilbertoAlvarez@users.noreply.github.com>
2019-08-07 13:56:05 +0200

    Update exeCRISPRfinder.sh

--- FindArrays/exeCRISPRfinder.sh
+++ FindArrays/exeCRISPRfinder.sh
@@ -1,1 +1,2 @@
-
+#after installing CRISPR Finder just run the perl script to find the CRISPR arrays
+perl CRISPRCasFinder.pl -in /home/gilberto/Documents/MTuber/Genomes/multiFasta.fa -out /home/gilberto/Documents/MTuber/Results_20G
