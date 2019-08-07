174584dd76876a993d5e4cd5d6daa6dc4b00c70f
Gilberto Álvarez <37610463+GilbertoAlvarez@users.noreply.github.com>
2019-08-07 15:32:51 +0200

    Update ScriptPython.py

--- DB/ScriptPython.py
+++ DB/ScriptPython.py
@@ -1,2 +1,15 @@
+from Bio import Entrez
+def singleEntry(singleID):   #the singleID is the accession number
+    handle = Entrez.efetch(db='nucleotide',id=singleID, rettype = 'fasta', retmode= 'text')
+    f = open('%s.fasta' % singleID, 'w')
+    f.write(handle.read())
+    handle.close()
+    f.close()
 
-aaldke
+
+    
+
+import pandas as pd
+df=pd.read_excel(r'/home/gabz/MTuber/evy153_Supp/Table_S1_List_of_strains.xlsx')
+print df
+
